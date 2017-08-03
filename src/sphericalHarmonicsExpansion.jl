98ce4f3c3fa32ff56e31b12cf7bae62bc4a1e5a1
mboberg <m.boberg@uke.de>
2017-08-03 18:07:11 +0200

    added description of sphericalHarmonicsExpansion()

--- src/sphericalHarmonicsExpansion.jl
+++ src/sphericalHarmonicsExpansion.jl
@@ -1,4 +1,11 @@
-#
+
+"""
+    sphericalHarmonicsExpansion(Clm::Array{Float64,1}, x::PolyVar{true}, y::PolyVar{true}, z::PolyVar{true})
+*Description:*  Calculation of the spherical harmonics expansion for given coefficients in Cartesian coordinates\\
+*Input:*  `Clm`       - Array with coefficients\\
+          `x, y, z`   - Cartesian coordinates\\
+*Output:*  Spherical harmonics expansion
+"""
 function sphericalHarmonicsExpansion(Clm::Array{Float64,1}, x::PolyVar{true}, y::PolyVar{true}, z::PolyVar{true})
 
   N = size(Clm,1)
