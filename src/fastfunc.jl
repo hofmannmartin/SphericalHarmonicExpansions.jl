7276e9d70e9dabdb11264ac8cc340826bfcce532
Martin Moeddel <mhofmann@uke.de>
2017-08-08 09:46:47 +0200

    update docstring

--- src/fastfunc.jl
+++ src/fastfunc.jl
@@ -6,29 +6,27 @@
     may violate strict IEEE semantics.
 
     # Examples
-    julia> using MultivariatePolynomials
+    julia> `using MultivariatePolynomials`
 
-    julia> @polyvar x y z
-    z
+    julia> `@polyvar x y z;`
 
-    julia> p = 15.0*x*y^2+7.5*x*z^13
-    7.5x*z^13 + 15.0x*y^2
-
-    julia> @fastfunc "foo" p
-    foo (generic function with 1 method)
-
-    julia> foo(1.0,2.0,3.0)
-    1.19574825e7
+    julia> `p = 15.0*x*y^2+7.5*x*z^13`;
+    
+    julia> `foo = @fastfunc p;`
+    
+    julia> `foo(1.0,2.0,3.0)`
+    `1.19574825e7`
     """
-macro fastfunc(functionname::String,polynomial)
-	polystr = string(eval(polynomial))
-	vars = eval(polynomial).x.vars
-	# insert "*" in between numbers and variables
-	regex = Regex("(\\d)" * replace(string(tuple(vars...)),", ","|"))
-	polystr = replace(polystr,regex,s"\1*\2")
-	# replace "+ -" with "- "
-	polystr = replace(polystr,r"([+])(\s)([-])",s"\3 ")
-	# create expression for function definition
-	expr = parse(functionname*string(tuple(vars...))*" = @fastmath "*polystr)
-	return eval(expr)
+macro fastfunc(polynomial)
+	return quote
+		local polystr = string($(esc(polynomial)))
+		local variables = string(tuple(vars($(esc(polynomial)))...))
+		# insert "*" in between numbers and variables
+		local regex = Regex("(\\d)" * replace(variables,", ","|"))
+		polystr = replace(polystr,regex,s"\1*\2")
+		# replace "+ -" with "- "
+		polystr = replace(polystr,r"([+])(\s)([-])",s"\3 ")
+		# create expression for function definition
+		eval(parse(variables*" -> @fastmath "*polystr))
+	end
 end
--- test/fastfunc.jl
+++ test/fastfunc.jl
@@ -1,13 +1,10 @@
-# TODO fix test
-#=
 @testset "macro for generating fast functions" begin
 	ε = eps(Float64)
 	f(x1,x2,x3) = 3*x3^2 + 2*x2*x1
 	@polyvar x y z
 	polynomial = 3*z^2 + 2*y*x
-	@multivarpoly2fastfunc "g" polynomial
+	g = @fastfunc polynomial
 	
 	t1,t2,t3 = randn(3)
 	@test isapprox(f(t1,t2,t3),g(t1,t2,t3),atol=ɛ)
 end
-=#
