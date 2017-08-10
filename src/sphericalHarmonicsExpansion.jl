effd35d4ded862c6cec220124d1698806bb8d63d
torb3n <torben.lobe@tuhh.de>
2017-08-10 15:35:23 +0200

    Added == and != operator

--- src/sphericalHarmonicsExpansion.jl
+++ src/sphericalHarmonicsExpansion.jl
@@ -18,7 +18,6 @@ type SphericalHarmonicCoefficients
       println("input vector needs o be of size (L+1)², where L ∈ ℕ₀.")
       throw(DomainError())
     end
-
     return new(zeros((L+1)^2),UInt(L))
   end
 end
@@ -31,6 +30,11 @@ import Base.setindex!
 setindex!(shc::SphericalHarmonicCoefficients,value,I) = setindex!(shc.c,value,I)
 setindex!(shc::SphericalHarmonicCoefficients,value,l,m) = setindex!(shc.c,value,l*(l+1)+m+1)
 
+import Base.==
+==(shca::SphericalHarmonicCoefficients, shcb::SphericalHarmonicCoefficients) = (shca.c == shcb.c ? true : false)
+
+import Base.!=
+!=(shca::SphericalHarmonicCoefficients, shcb::SphericalHarmonicCoefficients) = (shca.c != shcb.c ? true : false)
 """
     sphericalHarmonicsExpansion(Clm::Array{Float64,1}, x::PolyVar{true}, y::PolyVar{true}, z::PolyVar{true})
 *Description:*  Calculation of the spherical harmonics expansion in Cartesian coordinates
